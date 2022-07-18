7f0991e5afe26c4fb9c77faadfadf67e5536eaa0
Anaaag <agarc20@outlook.com>
2022-07-17 22:44:12 -0700

    implemented function for arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,20 @@
+const eqArrays = function(arrOne, arrTwo) {
+  if (arrOne.length !== arrTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] !== arrTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arrOne, arrTwo) {
+  if (eqArrays(arrOne, arrTwo)) {
+    console.log(`✅✅Assertion Passed: ${arrOne} === ${arrTwo}`);
+  } else {
+    console.log(`❌❌Assertion Failed: ${arrOne} !== ${arrTwo}`);
+  }
+};
+
