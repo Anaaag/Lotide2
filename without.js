84d37d6bf199ac409b87f0188b2d82580bb92a4f
Anaaag <agarc20@outlook.com>
2022-07-22 16:30:00 -0700

    created without function

---
+++ without.js
@@ -0,0 +1,31 @@
+const eqArrays = function(arrOne, arrTwo) {
+  if (arrOne.length !== arrTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] !== arrTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arrOne, arrTwo) {
+  if (eqArrays(arrOne, arrTwo)) {
+    console.log(`✅✅Assertion Passed: ${arrOne} === ${arrTwo}`);
+  } else {
+    console.log(`❌❌Assertion Failed: ${arrOne} !== ${arrTwo}`);
+  }
+};
+
+const without = function(source, itemsToRemove) {
+  let newArray = [];
+  for (let i = 0; i < source.length; i++) {
+    if (source[i] !== itemsToRemove[i]) 
+      newArray.push(source[i]);
+  }
+  return newArray;;
+}
+
+without([1, 2, 3], [1]); // => [2, 3]
+without(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
