0c390a05de1992ef23737804db6790afd55c7dca
Anaaag <agarc20@outlook.com>
2022-08-07 00:34:51 -0700

    implemented letterpostion function

---
+++ letterPosition.js
@@ -0,0 +1,40 @@
+const eqArrays = function(arrOne, arrTwo) {
+  if (arrOne.length !== arrTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] !== arrTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arrOne, arrTwo) {
+  if (eqArrays(arrOne, arrTwo)) {
+    console.log(`✅✅Assertion Passed: ${arrOne} === ${arrTwo}`);
+  } else {
+    console.log(`❌❌Assertion Failed: ${arrOne} !== ${arrTwo}`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== " ") {
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i);
+      } else {
+        results[sentence[i]] = [i];
+      }
+    }
+  }
+
+  return results;
+};
+
+
+assertArraysEqual(letterPositions("cat in hat").c, [0]);
+assertArraysEqual(letterPositions("cat in hat").h, [7]);
+assertArraysEqual(letterPositions("cat in hat").i, [4]);
+assertArraysEqual(letterPositions("cat in hat").n, [5]);
