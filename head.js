51b07ba913ff2ad9419e3c3b9a40f93cc8acdd55
Anaaag <agarc20@outlook.com>
2022-08-07 18:25:54 -0700

    cleaned up code

--- head.js
+++ head.js
@@ -1,19 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`❌❌❌ Assertion failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(arr) {
   let first = arr[0];
   return first;
 };
 
-
-//Test Case
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1]), 1);
-assertEqual(head([]));
+module.exports = head;
