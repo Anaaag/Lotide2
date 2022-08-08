71761546032ca430b113438f436e19794740b274
Anaaag <agarc20@outlook.com>
2022-08-07 18:15:10 -0700

    seperated assertEqual's tests

---
+++ test/assertEqualTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require('../assertEqual');
+
+//Test Code
+assertEqual("Lighthouse labs", "Bootcamp");
+assertEqual(1,1);
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(1, 1.5);
